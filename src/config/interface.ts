e585b3b8975d8944be18335bdf3f9ce855621a68
E-Dreamer <939902197@qq.com>
2023-03-21 12:14:54 +0800

    修改RouteObject中index的类型

--- src/config/interface.ts
+++ src/config/interface.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: E-Dreamer
  * @Date: 2022-08-04 09:05:44
- * @LastEditTime: 2022-08-04 11:30:25
+ * @LastEditTime: 2023-03-21 12:13:48
  * @LastEditors: E-Dreamer
  * @Description: 
  */
@@ -29,7 +29,7 @@ export interface RouteObject {
   caseSensitive?: boolean;
   children?: RouteObject[];
   element?: React.ReactNode;
-  index?: boolean;
+  index?: true & false;
   path?: string;
 	meta?: MetaProps;
 	isLink?: string;
